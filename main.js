7dba1f426d5122db303eb7d18c1fe1edae658cad
Kevin Yoon <jeldino@zigbang.com>
2022-08-19 12:53:40 +0900

    hi

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ function add(a, b) {
   return a + b;
 }
 
+function multiply(a, b) {
+  return a * b;
+}
+
 console.log(`hello world`);
 console.log(`add ${add(10, 20)}`);
+console.log(`multiply ${add(10, 20)}`);
